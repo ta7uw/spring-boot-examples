aa2b2acae870c14fcc33d44d2b790692fdee2029
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-12-15 23:49:32 +0900

    Add dependency to common

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,4 +11,12 @@ tasks {
 }
 
 dependencies {
+    implementation(project(":data"))
+    api("org.springframework.boot:spring-boot-starter")
+    api("org.springframework.boot:spring-boot-autoconfigure")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.3")
+
+    testApi("org.springframework.boot:spring-boot-starter-test")
 }
