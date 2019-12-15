58546cf2f1f10da8888fefcd0311690c258a3e85
Shimbo Takaaki <ta7uwtaka@gmail.com>
2019-12-15 23:48:52 +0900

    Add mybatis to data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,2 +1,16 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+tasks {
+    "jar"(Jar::class) {
+        enabled = true
+    }
+
+    "bootJar"(BootJar::class) {
+        enabled = false
+    }
+}
+
 dependencies {
+    api("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.1.0")
+    runtimeOnly("mysql:mysql-connector-java:8.0.16")
 }
