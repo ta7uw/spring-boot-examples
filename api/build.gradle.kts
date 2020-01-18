04f50ea0d0d8d9b0337596e7414094bba649e955
Shimbo Takaaki <ta7uwtaka@gmail.com>
2020-01-18 15:18:23 +0900

    Add SampleController to api

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
     implementation(project(":common"))
+    implementation(project(":data"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
--- api/src/main/java/ta7uw/examples/ApiApplication.java
+++ api/src/main/java/ta7uw/examples/ApiApplication.java
@@ -1,9 +1,11 @@
-package ta7uw.examples.api;
+package ta7uw.examples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
